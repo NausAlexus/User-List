d1ef773d127138759b78871d957e05ed7d5016d4
NausAlexus <naus.lexus@mail.ru>
2024-10-06 20:01:54 +0300

    First commit

---
+++ src/components/UserSearch/UserSearch.jsx
@@ -0,0 +1,51 @@
+import Skeleton from "../Skeleton/Skeleton";
+import "./UserSearch.css";
+import Users from "../Users/Users";
+
+const UserSearch = ({ onClickSendInvites, isLoading, item, searchValue, onChangeSearchValue, invites, onClickInvite }) => {
+    console.log(searchValue);
+    return (
+        <div className="container">
+            <div className="content">
+                <input
+                    value={searchValue}
+                    onChange={onChangeSearchValue}
+                    className="input_content"
+                    type="text"
+                    placeholder="Найти пользователя"
+                />
+                {isLoading ? (
+                    <div className="skeleton_list">
+                        <Skeleton />
+                        <Skeleton />
+                        <Skeleton />
+                        <Skeleton />
+                    </div>
+                ) : (
+                    <ul className="user_list">
+                        {item
+                            .filter((obj) => {
+                                const fullName = (obj.first_name + ' ' + obj.last_name).toLowerCase();
+
+                                return (
+                                    fullName.includes(searchValue.toLowerCase()) ||
+                                    obj.email.toLowerCase().includes(searchValue.toLowerCase())
+                                );
+                            })
+                            .map((obj) => (
+                                <Users onClickInvite={onClickInvite} isInvited = {invites.includes(obj.id)} key={obj.id} {...obj} />
+                            ))}
+                    </ul>
+                )}
+
+                <div className="btn">
+                    <button onClick={onClickSendInvites} className="btn_content">
+                        Отправить приглашение
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default UserSearch;
